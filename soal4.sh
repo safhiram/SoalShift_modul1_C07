a101ce5689185f4b81c6409a7fe56a36cdc65330
safhiram <37364680+safhiram@users.noreply.github.com>
2019-03-02 17:13:20 +0700

    Update soal4.sh

--- soal4.sh
+++ soal4.sh
@@ -15,3 +15,26 @@ low=$low$low
 up=$(echo $low | tr [a-z] [A-Z])
 
 cat $sislol | tr [${low:26}${up:26}] [${low:$hour:26}${up:$hour:26}] > "/home/safhiram/Sisop/$dat"
+
+--dekripsi
+#!/bin/bash
+
+#log=`cat syslog`
+#dat= `date "+%H:%M %d-%m-%Y"`
+
+read id
+#hour=$(date +"%H")
+#echo "$hour"
+#sislol=($(echo "$1"))
+#echo "$sislol"
+low=abcdefghijklmnopqrstuvwxyz
+
+low=$low$low
+
+#up=$(ord &log)
+up=$(echo $low | tr [a-z] [A-Z])
+
+#rot=$((26-${sislol[0]}))
+
+cat "$1" | tr "${up:id:26}" "${up:0:26}" | tr "${low:id:26}" "${low:0:26}" > "$1-balik"
+
